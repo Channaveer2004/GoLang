29d993b504d1ab056ffbc0368e76f3806a0e5fe4
Channaveer <channaveerbs0905@gmail.com>
2024-10-28 23:58:38 +0530

    vales

--- constants/constants.go
+++ constants/constants.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
-const s string = "constant"
+const s string = "constant"      // can be declared outside functions also
+const Apple_1 = "apple_1"        // typed constant
+const Apple_2 string = "apple_2" // untyped constant
 
 func main() {
-    fmt.Println(s)
+	fmt.Println(s)
+	fmt.Println(Apple_1)
+	fmt.Println(Apple_2)
 
-    const n = 500000000
+	const n = 500000000
+	const d = 3e20 / n
+	fmt.Println(d)
 
-    const d = 3e20 / n
-    fmt.Println(d)
+	fmt.Println(int64(d))
 
-    fmt.Println(int64(d))
+	fmt.Println(math.Sin(n))
 
-    fmt.Println(math.Sin(n))
+	const cantChange = "cant change const value once declared"
+	fmt.Println(cantChange)
+
+	const (
+		A int = 1
+		B     = 3.14
+		C     = "Hi!"
+	)
+
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
 }
--- values/values.go
+++ values/values.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Value from values.go!")
+	fmt.Println("Value from values.go!")
+
+	var i string = "Hello"
+	var j int = 15
+
+	fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("j has value: %v and type: %T", j, j)
 }
